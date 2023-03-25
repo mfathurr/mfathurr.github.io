4704554fe8a3543fc7c8b84cfb5076f53fc17f37
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-03-25 17:22:36 +0700

    Update hr style, add hr after result, & refactor script.js

--- progate/fe-dev-course/latihan-manipulasi-dom/script.js
+++ progate/fe-dev-course/latihan-manipulasi-dom/script.js
@@ -2,33 +2,40 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
   const input = document.getElementById("number").value;
-  console.log(typeof input);
-  const body = document.querySelector(".container");
+  const container = document.querySelector(".container");
   const result = document.createElement("p");
+  const hr = document.createElement("hr");
+
+  const showValue = () => {
+    container.append(result);
+    container.append(hr);
+  };
+
   const style = () => {
     result.style.fontSize = "1.5em";
     result.style.margin = "1rem";
   };
+
   if (Number(input) === 0) {
     result.innerHTML = `Input ${input}: invalid input`;
     result.style.color = "red";
     style();
-    body.append(result);
+    showValue();
   } else if (Number(input) % 3 === 0 && Number(input) % 5 === 0) {
     result.innerHTML = `Input ${input}: FizzBuzz!`;
     style();
-    body.append(result);
+    showValue();
   } else if (Number(input) % 3 === 0) {
     result.innerHTML = `Input ${input}: Fizz!`;
     style();
-    body.append(result);
+    showValue();
   } else if (Number(input) % 5 === 0) {
     result.innerHTML = `Input ${input}: Buzz!`;
     style();
-    body.append(result);
+    showValue();
   } else {
     result.innerHTML = Number(input);
     style();
-    body.append(result);
+    showValue();
   }
 });
